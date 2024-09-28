1a9320d43a47edba07cc350737a74936fab55faa
Sofa <muhammadsofa52@yahoo.com>
2024-09-28 15:42:25 +0700

    Retation City

--- app/Models/City.php
+++ app/Models/City.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class City extends Model
@@ -21,5 +23,20 @@ class City extends Model
     {
         $this->attributes['name'] = $value;
         $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(OfficeSpacePhoto::class);
+    }
+
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(OfficeSpaceBenefit::class);
+    }
+
+    public function officeSpaces(): BelongsTo
+    {
+        return $this->belongsTo(OfficeSpace::class);
     }
 }
