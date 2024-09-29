3ef6aaf19a4ec7fac37760e246306f719c66aa95
Sofa <muhammadsofa52@yahoo.com>
2024-09-29 23:25:54 +0700

    Form BookingTransactionResource

--- app/Filament/Resources/BookingTransactionResource.php
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -23,7 +23,27 @@ class BookingTransactionResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+                Forms\Components\TextInput::make('booking_trx_id')
+                ->required()
+                ->maxLength(255),
+
+                Forms\Components\TextInput::make('phone_number')
+                ->required()
+                ->maxLength(255),
+
+                Forms\Components\TextInput::make('total_amount')
+                ->required()
+                ->numeric()
+                ->prefix('IDR'),
+                
+                Forms\Components\TextInput::make('duration')
+                ->required()
+                ->numeric()
+                ->prefix('Days'),
             ]);
     }
 
