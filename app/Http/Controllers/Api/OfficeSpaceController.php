9edc6d78c100bf8fad50a21ecf9a2810d5ba1d0c
Sofa <muhammadsofa52@yahoo.com>
2024-09-30 11:31:24 +0700

    Show OfficeSpace Controller

--- app/Http/Controllers/Api/OfficeSpaceController.php
+++ app/Http/Controllers/Api/OfficeSpaceController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\OfficeSpaceResource;
 
 class OfficeSpaceController extends Controller
 {
-    //
+    public function index()
+    {
+        $officeSpaces = OfficeSpace::with(['city'])->get();
+        return OfficeSpaceResource::collection($officeSpaces);
+    }
+
+    public function show(OfficeSpace $officeSpace)
+    {
+        $officeSpace->load(['city', 'photos', 'benefits']);
+        return new OfficeSpaceResource($officeSpace);
+    }
 }
